8341c625a30c6d723578c93b15e0fcbb81083f8c
Taylor Miller <trmiller89@gmail.com>
2022-04-12 13:29:34 -0700

    Initial commit, add readme file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # todo
-it's a to-do app
+it's a to-do app [toe-dough]
+
+
+1. todo class
+  * title
+  * description
+  * dueDate
+  * priority
+  * notes
+  * checklist
+2. projects
+  * list of nested todos
+  * default project when first opening app
+  * create new projects
+  * choose which project todos are part of
+3. keep app logic and DOM stuff separate
+
+
+UI
+
+a. view all projects
+b. view all todos in each project (show title, dueDate, priority)
+c. expand a single todo to see/edit its details
+d. delete a todo (with confirmation)
+
+
+storage
+
+use localStorage to get/put JSON
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-Do List',
+      template: './src/index.template.html',
+      inject: 'body',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
